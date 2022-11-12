18cd9742f3bb5fb05904e593a1e95a63ec17e77e
bohendo <bohendo@pm.me>
2022-11-12 00:04:04 -0500

    add nvidia drivers to system76

--- nixos/system/system76-configuration.nix
+++ nixos/system/system76-configuration.nix
@@ -9,4 +9,9 @@
 
   networking.hostName = "system76";
 
+  services.xserver.videoDriver = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
 }
