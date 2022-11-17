b0ecf4442c0758916dfc2bf0aa67d1c5cb481346
bohendo <bohendo@pm.me>
2022-11-16 22:38:49 -0500

    put user config back in nixos & restore vimrc

---
+++ darwin/user/editors.nix
@@ -0,0 +1,24 @@
+{ config, inputs, pkgs, ... }:
+{
+  programs = {
+
+    neovim = {
+      enable = true;
+      extraConfig = ''
+        ${builtins.readFile ../../vimrc}
+      '';
+      plugins = with pkgs.vimPlugins; [
+        nerdtree
+        supertab
+        syntastic
+        vim-addon-nix
+        vim-javascript
+        vim-jsx-typescript
+        vim-nix
+        vim-solidity
+      ];
+      vimAlias = true;
+    };
+
+  };
+}
