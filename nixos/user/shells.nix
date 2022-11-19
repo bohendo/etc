8904d65a2d6cbe668eabb2d60d5b9d688b1dddfa
bohendo <bohendo@pm.me>
2022-11-19 16:30:17 -0500

    add prompt & local bin to nixos shell

--- nixos/user/shells.nix
+++ nixos/user/shells.nix
@@ -9,8 +9,14 @@
       historyFileSize = 20000;
       historyIgnore = [ "ls" ];
       sessionVariables = {
-        EDITOR = "nvim";
+        EDITOR = "${pkgs.neovim}/bin/nvim";
+        ETC_DIR = "/Users/bohendo/etc";
+        SHELL = "${pkgs.bash}/bin/bash";
       };
+      profileExtra = ''
+        PS1='\n''${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ ';
+        PATH="$PATH:/Users/bohendo/bin"
+      '';
       shellAliases = {
         ".." = "cd ..";
         "..." = "cd ../..";
