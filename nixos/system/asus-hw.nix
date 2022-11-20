68b03c4f4e18b1e4dc1ba6043f29bf8fcc0e351e
Bo Henderson <bohende@gmail.com>
2022-11-20 14:12:50 -0500

    refresh asus hardware config

--- nixos/system/asus-hw.nix
+++ nixos/system/asus-hw.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -20,27 +20,37 @@
 
   boot.initrd.luks.devices."luks-26758ad4-8bfd-45f8-8d3b-8ae4041f5a9a".device = "/dev/disk/by-uuid/26758ad4-8bfd-45f8-8d3b-8ae4041f5a9a";
 
+  fileSystems."/nix/store" =
+    { device = "/nix/store";
+      fsType = "none";
+      options = [ "bind" ];
+    };
+
   fileSystems."/boot/efi" =
     { device = "/dev/disk/by-uuid/8B1A-5266";
       fsType = "vfat";
     };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/48091eef-9820-437a-b9e5-54bbe4850e88"; }
-    ];
 
   fileSystems."/mnt/disk" =
     { device = "/dev/disk/by-uuid/669bb023-6968-45ed-9e1a-aaf1e3710bd3";
       fsType = "ext4";
     };
 
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/8e527956-d397-4c86-b456-8ca885a3e12b"; }
+    ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker_gwbridge.useDHCP = lib.mkDefault true;
+  # networking.interfaces.veth1e3bb92.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
