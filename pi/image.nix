5c2d4573c26d21b6103fb0a763ee952ec023c65c
Bo Henderson <bohende@gmail.com>
2022-11-21 17:19:33 -0500

    init flake for building pi sd image

--- pi/image.nix
+++ pi/image.nix
@@ -4,6 +4,8 @@
     "${modulesPath}/installer/sd-card/sd-image-armv7l-multiplatform.nix"
   ];
 
+  system.stateVersion = "22.05";
+
   # Needed for https://github.com/NixOS/nixpkgs/issues/58959
   boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
 }
