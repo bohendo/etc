6992021d620407e2d966c023197a884cacd65b9d
bohendo <bohendo@pm.me>
2022-11-21 12:59:38 -0500

    import nixos-hardware module for system76

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/1f4c790f716a7bad8bb000e5cf07e3b5d36a48b8";
-    home-manager.url = "github:nix-community/home-manager/master";
+    nixos-hardware.url = "github:nixos/nixos-hardware/1108c1b8614017c8b52005054fd27a00e4feb51b";
+    home-manager.url = "github:nix-community/home-manager/433e8de330fd9c157b636f9ccea45e3eeaf69ad2";
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs:
@@ -42,6 +43,7 @@
         system76 = lib.nixosSystem {
           inherit system;
           modules = [
+            inputs.nixos-hardware.nixosModules.system76
             ./system/system76.nix
           ];
           specialArgs = { inherit inputs; };
