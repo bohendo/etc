8aace5215a0dc9c67d43e1ddcfa39fe338278472
Bo Henderson <bohende@gmail.com>
2022-11-21 16:25:55 -0500

    allow armv7l emulation

--- pi/image.nix
+++ pi/image.nix
@@ -1,11 +1,8 @@
 {
   pkgs, modulesPath, lib, ... }: {
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/sd-card/sd-image-armv7l-multiplatform.nix"
   ];
-
-  # use the latest Linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Needed for https://github.com/NixOS/nixpkgs/issues/58959
   boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
