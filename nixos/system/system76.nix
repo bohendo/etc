7cc68b38e266ea584b208dca4956c600f8a71bf9
bohendo <bohendo@pm.me>
2022-11-23 16:50:22 -0500

    add kvm modprobe config

--- nixos/system/system76.nix
+++ nixos/system/system76.nix
@@ -10,8 +10,15 @@
 
   networking.hostName = "system76";
 
-  virtualisation.virtualbox.host.enable = true;
   virtualisation.libvirtd.enable = true;
+  users.extraGroups.libvirtd.members = [ "bohendo" ];
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1
+  '';
+
+  virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "bohendo" ];
 
 }
